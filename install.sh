89b4ed34d1f0ade94144873e7f17eb5a62ba7c5e
Pablo Fonseca <pablofonseca777@gmail.com>
2015-12-12 18:16:20 -0200

    Updated install.sh and some plugins

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Install HomeBrew
+# Install command line tools
+xcode-select --install
+
+# HomeBrew
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# Update brew
 brew update
+brew install caskroom/cask/brew-cask
 
-# Install vim, tmux, git, node, mysql and postgresql
 brew install vim tmux git node mysql postgresql
 
-# Other tools
-brew install zsh zsh-completions the_silver_searcher openssl redis tmate wget
+brew install zsh zsh-completions the_silver_searcher openssl redis tmate wget heroku-toolbelt
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -24,21 +24,7 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 \curl -sSL https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
 type rvm | head -n 1
-
-# Cask - http://caskroom.io/
-brew install caskroom/cask/brew-cask
-brew cask install google-chrome
-brew cask install alfred
-brew cask install android-file-transfer
-brew cask install caffeine
-brew cask install cheatsheet
-brew cask install dropbox
-brew cask install google-drive
-brew cask install google-hangouts
-brew cask install onepassword
-brew cask install spectacle
-brew cask install qlmarkdown
-brew cask install quicklook-json
+rvm install ruby --latest
 
 # Vim and Tmux
 ln -s dotfiles/zsh/zshrc ~/.zshrc
@@ -52,6 +38,29 @@ git clone https://github.com/tmux-plugins/tmux-battery ~/.tmux/plugins/tmux-batt
 
 # Git
 ln -s dotfiles/.gitconfig ~/.gitconfig
+
+# Cask - http://caskroom.io/
+brew cask install iterm2
+brew cask install google-chrome
+brew cask install alfred
+brew cask install android-file-transfer
+brew cask install caffeine
+brew cask install cheatsheet
+brew cask install dropbox
+brew cask install google-drive
+brew cask install google-hangouts
+brew cask install onepassword
+brew cask install spectacle
+brew cask install qlmarkdown
+brew cask install quicklook-json
+brew cask install slack
+brew cask install screenhero
+brew cask install skype
+brew cask install spotify
+
+# Clean up
+brew cleanup
+brew cask cleanup
 
 # NeoBundle
 cd
