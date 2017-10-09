825b7f62e5b8b2bc2c413a417e56143fe36f5530
Pablo Fonseca <pablofonseca777@gmail.com>
2017-10-09 15:02:00 -0300

    Add new fzf functions

--- zsh/config/fzf.zsh
+++ zsh/config/fzf.zsh
@@ -8,16 +8,29 @@ fhist() {
 
 bindkey -s '^r' 'fhist\n'
 
-# fe [FUZZY PATTERN] - Open the selected file with the default editor
-#   - Bypass fuzzy finder if there's only one match (--select-1)
-#   - Exit if there's no match (--exit-0)
-fe() {
-  local files
-  IFS=$'\n' files=($(cd spec; fzf-tmux --query="$1" --multi --select-1 --exit-0))
-  [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
+# Modified version where you can press
+# CTRL-O to open with `open` command
+# CTRL-E or Enter key to open with the $EDITOR
+fo() {
+  local out file key
+  IFS=$'\n' out=($(fzf-tmux --query="$1" --exit-0 --expect=ctrl-o,ctrl-e))
+  key=$(head -1 <<< "$out")
+  file=$(head -2 <<< "$out" | tail -1)
+  if [ -n "$file" ]; then
+    [ "$key" = ctrl-o ] && open "$file" || ${EDITOR:-vim} "$file"
+  fi
 }
+bindkey -s '^t' 'fo\n'
 
-bindkey -s '^t' 'fe\n'
+fspec() {
+  local out file key
+  IFS=$'\n' out=($(fzf-tmux --query="$1" --exit-0))
+  key=$(head -1 <<< "$out")
+  file=$(head -2 <<< "$out" | tail -1)
+  if [ -n "$file" ]; then
+    bin/rspec "$file"
+  fi
+}
 
 # cdf - cd into the directory of the selected file
 cdf() {
