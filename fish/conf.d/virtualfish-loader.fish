7318d63858d4ab39e0ebe534087028c08533daf8
Pablo Fonseca <pablofonseca777@gmail.com>
2020-06-10 23:20:56 -0300

    fish stuff 2

---
+++ fish/conf.d/virtualfish-loader.fish
@@ -0,0 +1,4 @@
+set -g VIRTUALFISH_VERSION 2.2.5
+set -g VIRTUALFISH_PYTHON_EXEC /Library/Developer/CommandLineTools/usr/bin/python3
+source /Users/pablobfonseca/Library/Python/3.7/lib/python/site-packages/virtualfish/virtual.fish
+emit virtualfish_did_setup_plugins
