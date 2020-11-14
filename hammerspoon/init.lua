6709f235601678086e0770266b7bf9c9a46a0125
Pablo Fonseca <pablofonseca777@gmail.com>
2020-11-14 13:38:47 -0300

    adds hammerspoon config

---
+++ hammerspoon/init.lua
@@ -0,0 +1,31 @@
+local hyper = {'ctrl', 'alt', 'cmd'}
+local master = {'ctrl', 'cmd'}
+
+hs.hotkey.bind(hyper, 'r', function()
+  hs.reload()
+end)
+hs.alert.show('Config loaded!')
+
+hs.loadSpoon('MiroWindowsManager')
+
+hs.window.animationDuration = 0.3
+spoon.MiroWindowsManager:bindHotkeys({
+  up = {master, 'k'},
+  right = {master, 'l'},
+  down = {master, 'j'},
+  left = {master, 'h'},
+  fullscreen = {master, 'o'}
+})
+
+function focusApp(key, appName)
+   return hs.hotkey.bind(master, key, function()
+     return hs.application.launchOrFocus(appName)
+   end)
+end
+
+focusApp('b', 'Google Chrome')
+focusApp('e', 'Emacs')
+focusApp('t', 'iTerm')
+focusApp('f', 'Firefox')
+focusApp('s', 'Slack')
+focusApp('v', 'Stremio')
