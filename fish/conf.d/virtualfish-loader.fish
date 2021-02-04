fe74fd5ca179e999fe5895993e0dd7a06ea837e2
Pablo Fonseca <pablofonseca777@gmail.com>
2021-02-04 15:37:03 -0300

    fish updates

--- fish/conf.d/virtualfish-loader.fish
+++ fish/conf.d/virtualfish-loader.fish
@@ -1,4 +1,4 @@
-set -g VIRTUALFISH_VERSION 2.2.5
-set -g VIRTUALFISH_PYTHON_EXEC /Library/Developer/CommandLineTools/usr/bin/python3
-source /Users/pablobfonseca/Library/Python/3.7/lib/python/site-packages/virtualfish/virtual.fish
+set -g VIRTUALFISH_VERSION 2.5.1
+set -g VIRTUALFISH_PYTHON_EXEC /usr/local/opt/python@3.8/bin/python3.8
+source /usr/local/lib/python3.8/site-packages/virtualfish/virtual.fish
 emit virtualfish_did_setup_plugins
