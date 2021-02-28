ea1edfbfe236a24862259c635fba70deeb6de26f
Pablo Fonseca <pablofonseca777@gmail.com>
2021-02-27 22:52:58 -0300

    remove unused plugins

--- fish/conf.d/virtualfish-loader.fish
+++ fish/conf.d/virtualfish-loader.fish
@@ -1,4 +1,6 @@
 set -g VIRTUALFISH_VERSION 2.5.1
 set -g VIRTUALFISH_PYTHON_EXEC /usr/local/opt/python@3.8/bin/python3.8
 source /usr/local/lib/python3.8/site-packages/virtualfish/virtual.fish
+source /usr/local/lib/python3.8/site-packages/virtualfish/projects.fish
+source /usr/local/lib/python3.8/site-packages/virtualfish/auto_activation.fish
 emit virtualfish_did_setup_plugins
