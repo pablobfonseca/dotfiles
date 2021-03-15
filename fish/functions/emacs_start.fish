f79d4b6d828357e07c562405c58be18109c5697e
Pablo Fonseca <pablofonseca777@gmail.com>
2021-03-15 15:19:31 -0300

    some fish and vim updates

--- fish/functions/emacs_start.fish
+++
@@ -1,7 +0,0 @@
-function emacs_start
-  if not pgrep -f emacs > /dev/null
-    emacs --daemon && emacsclient -c
-  else
-    emacsclient -c
-  end
-end
--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,21 +1,33 @@
+local wm = require ('window-management')
+
 local hyper = {'ctrl', 'alt', 'cmd'}
 local master = {'ctrl', 'cmd'}
+local alt = {'cmd', 'alt'}
 
 hs.hotkey.bind(hyper, 'r', function()
   hs.reload()
 end)
 hs.alert.show('Config loaded!')
 
-hs.loadSpoon('MiroWindowsManager')
+hs.hotkey.bind(master, 'l', function() 
+  wm.moveWindowToPosition(wm.screenPositions.right)
+end)
 
-hs.window.animationDuration = 0.3
-spoon.MiroWindowsManager:bindHotkeys({
-  up = {master, 'k'},
-  right = {master, 'l'},
-  down = {master, 'j'},
-  left = {master, 'h'},
-  fullscreen = {master, 'o'}
-})
+hs.hotkey.bind(master, 'h', function() 
+  wm.moveWindowToPosition(wm.screenPositions.left)
+end)
+
+hs.hotkey.bind(master, 'j', function() 
+  wm.moveWindowToPosition(wm.screenPositions.bottom)
+end)
+
+hs.hotkey.bind(master, 'k', function() 
+  wm.moveWindowToPosition(wm.screenPositions.top)
+end)
+
+hs.hotkey.bind(master, 'o', function() 
+  wm.windowMaximize(0)
+end)
 
 function focusApp(key, appName)
    return hs.hotkey.bind(master, key, function()
@@ -23,10 +35,17 @@ function focusApp(key, appName)
    end)
 end
 
+hs.hotkey.bind(alt, "l", function()
+  hs.window.focusedWindow():moveOneScreenEast()
+end)
+
+hs.hotkey.bind(alt, "h", function()
+  hs.window.focusedWindow():moveOneScreenWest()
+end)
+
 focusApp('b', 'Google Chrome')
 focusApp('e', 'Emacs')
 focusApp('t', 'iTerm')
-focusApp('f', 'Finder')
+focusApp('f', 'Firefox')
 focusApp('s', 'Slack')
 focusApp('v', 'Stremio')
-focusApp('i', 'Linear')
