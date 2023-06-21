2847d0f176474598e8221d396edd0f1654af546d
Pablo Fonseca <pablofonseca777@gmail.com>
2023-06-21 00:46:43 -0300

    fix(hammerspoon): remove Emacs' shortcut

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,51 +1,50 @@
-local wm = require ('window-management')
+local wm = require("window-management")
 
-local hyper = {'ctrl', 'alt', 'cmd'}
-local master = {'ctrl', 'cmd'}
-local alt = {'cmd', 'alt'}
+local hyper = { "ctrl", "alt", "cmd" }
+local master = { "ctrl", "cmd" }
+local alt = { "cmd", "alt" }
 
-hs.hotkey.bind(hyper, 'r', function()
-  hs.reload()
+hs.hotkey.bind(hyper, "r", function()
+	hs.reload()
 end)
-hs.alert.show('Config loaded!')
+hs.alert.show("Config loaded!")
 
-hs.hotkey.bind(master, 'l', function() 
-  wm.moveWindowToPosition(wm.screenPositions.right)
-end)
-
-hs.hotkey.bind(master, 'h', function() 
-  wm.moveWindowToPosition(wm.screenPositions.left)
+hs.hotkey.bind(master, "l", function()
+	wm.moveWindowToPosition(wm.screenPositions.right)
 end)
 
-hs.hotkey.bind(master, 'j', function() 
-  wm.moveWindowToPosition(wm.screenPositions.bottom)
+hs.hotkey.bind(master, "h", function()
+	wm.moveWindowToPosition(wm.screenPositions.left)
 end)
 
-hs.hotkey.bind(master, 'k', function() 
-  wm.moveWindowToPosition(wm.screenPositions.top)
+hs.hotkey.bind(master, "j", function()
+	wm.moveWindowToPosition(wm.screenPositions.bottom)
 end)
 
-hs.hotkey.bind(master, 'o', function() 
-  wm.windowMaximize(0)
+hs.hotkey.bind(master, "k", function()
+	wm.moveWindowToPosition(wm.screenPositions.top)
+end)
+
+hs.hotkey.bind(master, "o", function()
+	wm.windowMaximize(0)
 end)
 
 function focusApp(key, appName)
-   return hs.hotkey.bind(master, key, function()
-     return hs.application.launchOrFocus(appName)
-   end)
+	return hs.hotkey.bind(master, key, function()
+		return hs.application.launchOrFocus(appName)
+	end)
 end
 
 hs.hotkey.bind(alt, "l", function()
-  hs.window.focusedWindow():moveOneScreenEast()
+	hs.window.focusedWindow():moveOneScreenEast()
 end)
 
 hs.hotkey.bind(alt, "h", function()
-  hs.window.focusedWindow():moveOneScreenWest()
+	hs.window.focusedWindow():moveOneScreenWest()
 end)
 
-focusApp('b', 'Google Chrome')
-focusApp('e', 'Emacs')
-focusApp('t', 'iTerm')
-focusApp('f', 'Firefox')
-focusApp('s', 'Slack')
-focusApp('v', 'Stremio')
+focusApp("b", "Google Chrome")
+focusApp("t", "iTerm")
+focusApp("f", "Firefox")
+focusApp("s", "Slack")
+focusApp("v", "Stremio")
