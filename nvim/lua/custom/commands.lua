c82db3cac67ad5f04ca11360916bc2663e09e648
Pablo Fonseca <pablofonseca777@gmail.com>
2023-06-23 15:29:27 -0300

    fix(nvim): do not show diagnostics on hover

--- nvim/lua/custom/commands.lua
+++ nvim/lua/custom/commands.lua
@@ -1,5 +1,5 @@
 -- Open diagnostics in a floating window
-vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float({border="rounded", focus=false})]]
+-- vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float({border="rounded", focus=false})]]
 
 -- creates a command that looks for a buffer called restmode, if it does not exists, create a new buffer setting the filetype to http and name the buffer restmode
 vim.cmd [[command! -nargs=0 RestMode execute 'edit restmode' | set filetype=http]]
