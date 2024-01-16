e19bfa4762e5d15efa9c9c9187691eeb819fa040
Pablo Fonseca <pablofonseca777@gmail.com>
2024-01-16 08:03:31 +0000

    feat(nvim): add command to copy the file contents

--- nvim/custom/commands.lua
+++ nvim/custom/commands.lua
@@ -2,3 +2,6 @@
 -- vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float({border="rounded", focus=false})]]
 
 vim.cmd [[command! -nargs=0 RestMode execute 'edit restmode' | set filetype=http buftype=nofile]]
+
+-- create a command to copy the entire file content
+vim.cmd [[command! -nargs=0 CopyFile execute 'normal! ggVGy']]
