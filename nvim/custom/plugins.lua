fe81f84525a1dd07592cc847870d9384243b4bbb
Pablo Fonseca <pablofonseca777@gmail.com>
2024-01-17 15:36:26 +0000

    fix(nvim): replace null-ls with none-ls

--- nvim/custom/plugins.lua
+++ nvim/custom/plugins.lua
@@ -10,7 +10,7 @@ local plugins = {
     dependencies = {
       -- format & linting
       {
-        "jose-elias-alvarez/null-ls.nvim",
+        "nvimtools/none-ls.nvim",
         config = function()
           require "custom.configs.null-ls"
         end,
