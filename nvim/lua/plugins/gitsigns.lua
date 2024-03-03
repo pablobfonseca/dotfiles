eebaf43cdbb52f063635e002963e87afa8d5b3c1
Pablo Fonseca <pablofonseca777@gmail.com>
2024-03-03 23:33:17 +0000

    fix(nvim): add more mappings for gitsigns

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -19,9 +19,31 @@ return {
           vim.keymap.set(mode, l, r, opts)
         end
 
+        map("n", "]c", function()
+          if vim.wo.diff then
+            return "]c"
+          end
+          vim.schedule(function()
+            gs.next_hunk()
+          end)
+          return "<Ignore>"
+        end, { expr = true })
+
+        map("n", "[c", function()
+          if vim.wo.diff then
+            return "[c"
+          end
+          vim.schedule(function()
+            gs.prev_hunk()
+          end)
+          return "<Ignore>"
+        end, { expr = true })
+
         map("n", "<leader>b", function()
           gs.blame_line { full = true }
         end)
+
+        map("n", "<leader>gs", gs.stage_buffer)
       end,
     }
   end,
