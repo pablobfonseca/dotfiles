a35d7ef0b4f45719c909327fcd9ee184626d79aa
Pablo Fonseca <pablofonseca777@gmail.com>
2024-03-03 23:32:35 +0000

    feat(nvim): adds neorg

---
+++ nvim/lua/plugins/neorg.lua
@@ -0,0 +1,29 @@
+return {
+  "nvim-neorg/neorg",
+  ft = "norg",
+  build = ":Neorg sync-parsers",
+  dependencies = { { "nvim-lua/plenary.nvim" }, { "nvim-neorg/neorg-telescope" } },
+  keys = {
+    { "n", "<C-x>w", mode = "n", desc = "Change neorg workspace", "<cmd>Telescope neorg switch_workspace<cr>" },
+  },
+  opts = {
+    load = {
+      ["core.defaults"] = {},
+      ["core.concealer"] = {},
+      ["core.completion"] = {
+        config = {
+          engine = "nvim-cmp",
+        },
+      },
+      ["core.integrations.telescope"] = {},
+      ["core.dirman"] = {
+        config = {
+          workspaces = {
+            notes = "~/Dropbox/notes",
+            dotfiles = "~/.dotfiles",
+          },
+        },
+      },
+    },
+  },
+}
