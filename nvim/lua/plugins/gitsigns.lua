bd80a56a578375c9ef94d7ca7f28b1ca0a93f172
Pablo Fonseca <pablofonseca777@gmail.com>
2024-03-03 22:15:20 +0000

    fix(nvim): fix gitsigns

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,24 +1,28 @@
 return {
   "lewis6991/gitsigns.nvim",
-  event = "User FilePost",
-  keys = {
-    {
-      "<leader>b",
-      mode = "n",
-      desc = "Blame line",
-      function()
-        require("gitsigns").blame_line()
+  config = function()
+    require("gitsigns").setup {
+      signs = {
+        add = { text = "│" },
+        change = { text = "│" },
+        delete = { text = "󰍵" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+        untracked = { text = "│" },
+      },
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        map("n", "<leader>b", function()
+          gs.blame_line { full = true }
+        end)
       end,
-    },
-  },
-  opts = {
-    signs = {
-      add = { text = "│" },
-      change = { text = "│" },
-      delete = { text = "󰍵" },
-      topdelete = { text = "‾" },
-      changedelete = { text = "~" },
-      untracked = { text = "│" },
-    },
-  },
+    }
+  end,
 }
