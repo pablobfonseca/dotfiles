8a081db2a50fbcce008a192271d8faed9080ed4d
Pablo Fonseca <pablofonseca777@gmail.com>
2024-03-04 23:22:51 +0000

    fix(nvim): customize bufferline

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -3,9 +3,13 @@ return {
   version = "*",
   lazy = false,
   keys = {
-    { "<Tab>", mode = "n", ":bnext<cr>", desc = "Next buffer" },
+    { "<Tab>",   mode = "n", ":bnext<cr>",     desc = "Next buffer" },
     { "<S-Tab>", mode = "n", ":bprevious<cr>", desc = "Previous buffer" },
   },
   dependencies = "nvim-tree/nvim-web-devicons",
-  opts = {},
+  opts = {
+    options = {
+      separator_style = "slant",
+    },
+  },
 }
