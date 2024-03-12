731da4171c9e46770e8cffaff8699a796ac6db2b
Pablo Fonseca <pablofonseca777@gmail.com>
2024-03-12 09:08:03 +0000

    feat(nvim): add second_brain workspace for neorg

--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -4,8 +4,9 @@ return {
   build = ":Neorg sync-parsers",
   dependencies = { { "nvim-lua/plenary.nvim" }, { "nvim-neorg/neorg-telescope" } },
   keys = {
-    { "<C-c>nw", mode = "n", desc = "Change neorg workspace", "<cmd>Telescope neorg switch_workspace<cr>" },
-    { "<C-c>nt", mode = "n", desc = "Toggle table of contents", "<cmd>Neorg toc<cr>" },
+    { "<C-c>nw", mode = "n", desc = "Change neorg workspace",     "<cmd>Telescope neorg switch_workspace<cr>" },
+    { "<C-c>nt", mode = "n", desc = "Toggle table of contents",   "<cmd>Neorg toc<cr>" },
+    { "<C-c>nf", mode = "n", desc = "Telescope find neorg files", "<cmd>Telescope neorg find_norg_files<cr>" },
   },
   opts = {
     load = {
@@ -26,6 +27,7 @@ return {
           workspaces = {
             notes = "~/Dropbox/notes",
             dotfiles = "~/.dotfiles",
+            second_brain = "~/Dropbox/second_brain",
           },
         },
       },
