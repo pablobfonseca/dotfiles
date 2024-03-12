c50d1bc847aa622824bc41282b71222aafe08a20
Pablo Fonseca <pablofonseca777@gmail.com>
2024-03-12 11:22:55 +0000

    fix(nvim,tmux,zsh): few changes

--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -4,18 +4,14 @@ return {
   build = ":Neorg sync-parsers",
   dependencies = { { "nvim-lua/plenary.nvim" }, { "nvim-neorg/neorg-telescope" } },
   keys = {
-    { "<C-c>nw", mode = "n", desc = "Change neorg workspace",     "<cmd>Telescope neorg switch_workspace<cr>" },
-    { "<C-c>nt", mode = "n", desc = "Toggle table of contents",   "<cmd>Neorg toc<cr>" },
+    { "<C-c>nw", mode = "n", desc = "Change neorg workspace", "<cmd>Telescope neorg switch_workspace<cr>" },
+    { "<C-c>nt", mode = "n", desc = "Toggle table of contents", "<cmd>Neorg toc<cr>" },
     { "<C-c>nf", mode = "n", desc = "Telescope find neorg files", "<cmd>Telescope neorg find_norg_files<cr>" },
   },
   opts = {
     load = {
       ["core.defaults"] = {},
-      ["core.concealer"] = {
-        config = {
-          folds = false,
-        },
-      },
+      ["core.concealer"] = {},
       ["core.completion"] = {
         config = {
           engine = "nvim-cmp",
