b90867a28b3668e9cfe290661129dcbb5042468c
Pablo Fonseca <pablofonseca777@gmail.com>
2024-03-26 16:14:13 +0000

    feat(hammerspoon): adds hotkey for Firefox

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -5,42 +5,42 @@ local master = { "ctrl", "cmd" }
 local alt = { "cmd", "alt" }
 
 hs.hotkey.bind(hyper, "r", function()
-	hs.reload()
+  hs.reload()
 end)
 hs.alert.show("Config loaded!")
 
 hs.hotkey.bind(master, "l", function()
-	wm.moveWindowToPosition(wm.screenPositions.right)
+  wm.moveWindowToPosition(wm.screenPositions.right)
 end)
 
 hs.hotkey.bind(master, "h", function()
-	wm.moveWindowToPosition(wm.screenPositions.left)
+  wm.moveWindowToPosition(wm.screenPositions.left)
 end)
 
 hs.hotkey.bind(master, "j", function()
-	wm.moveWindowToPosition(wm.screenPositions.bottom)
+  wm.moveWindowToPosition(wm.screenPositions.bottom)
 end)
 
 hs.hotkey.bind(master, "k", function()
-	wm.moveWindowToPosition(wm.screenPositions.top)
+  wm.moveWindowToPosition(wm.screenPositions.top)
 end)
 
 hs.hotkey.bind(master, "o", function()
-	wm.windowMaximize(0)
+  wm.windowMaximize(0)
 end)
 
 function focusApp(key, appName)
-	return hs.hotkey.bind(master, key, function()
-		return hs.application.launchOrFocus(appName)
-	end)
+  return hs.hotkey.bind(master, key, function()
+    return hs.application.launchOrFocus(appName)
+  end)
 end
 
 hs.hotkey.bind(alt, "l", function()
-	hs.window.focusedWindow():moveOneScreenEast()
+  hs.window.focusedWindow():moveOneScreenEast()
 end)
 
 hs.hotkey.bind(alt, "h", function()
-	hs.window.focusedWindow():moveOneScreenWest()
+  hs.window.focusedWindow():moveOneScreenWest()
 end)
 
 focusApp("b", "Arc")
@@ -48,3 +48,4 @@ focusApp("t", "iTerm")
 focusApp("w", "Warp")
 focusApp("s", "Slack")
 focusApp("n", "Obsidian")
+focusApp("f", "Firefox")
