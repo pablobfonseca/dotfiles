36bee02617ddadc06574e39ff3ec452f1441090f
Pablo Fonseca <pablofonseca777@gmail.com>
2024-04-16 18:43:33 +0100

    fix(nvim): move diffview to its own file and add some mappings

---
+++ nvim/lua/plugins/diffview.lua
@@ -0,0 +1,17 @@
+return {
+  "sindrets/diffview.nvim",
+  keys = {
+    {
+      "<C-c>Do",
+      mode = "n",
+      desc = "Diff View Open",
+      "<cmd>DiffviewOpen<cr>",
+    },
+    {
+      "<C-c>Dc",
+      mode = "n",
+      desc = "Diff View Close",
+      "<cmd>DiffviewOpen<cr>",
+    },
+  },
+}
--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -6,7 +6,6 @@ return {
   },
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "sindrets/diffview.nvim",
     "nvim-telescope/telescope.nvim",
   },
   opts = {
