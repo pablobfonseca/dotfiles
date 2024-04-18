30fdcd4617f5ab02fd0b0693845c563861de0f90
Pablo Fonseca <pablofonseca777@gmail.com>
2024-04-18 12:01:12 +0100

    fix(nvim): add notification for octo pr merge

--- nvim/lua/plugins/octo.lua
+++ nvim/lua/plugins/octo.lua
@@ -7,14 +7,22 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   keys = {
-    { "<C-c>ors", mode = "n", desc = "[O]cto [R]eview [S]tart", "<cmd>Octo review start<cr>" },
+    { "<C-c>ors", mode = "n", desc = "[O]cto [R]eview [S]tart",  "<cmd>Octo review start<cr>" },
     { "<C-c>orS", mode = "n", desc = "[O]cto [R]eview [S]ubmit", "<cmd>Octo review submit<cr>" },
-    { "<C-c>opm", mode = "n", desc = "[O]cto [P]R [M]erge", "<cmd>Octo pr merge<cr>" },
-    { "<C-c>opl", mode = "n", desc = "[O]cto [P]R [L]ist", "<cmd>Octo pr list<cr>" },
-    { "<C-c>opc", mode = "n", desc = "[O]cto [P]R [C]reate", "<cmd>Octo pr create<cr>" },
+    {
+      "<C-c>opm",
+      mode = "n",
+      desc = "[O]cto [P]R [M]erge",
+      function()
+        require("octo.commands").merge_pr "commit"
+        vim.notify("PR Merged", vim.log.levels.INFO)
+      end,
+    },
+    { "<C-c>opl", mode = "n", desc = "[O]cto [P]R [L]ist",       "<cmd>Octo pr list<cr>" },
+    { "<C-c>opc", mode = "n", desc = "[O]cto [P]R [C]reate",     "<cmd>Octo pr create<cr>" },
     { "<C-c>oil", mode = "n", desc = "[O]cto My [I]ssue [L]ist", "<cmd>Octo issue list assignee=pablobfonseca<cr>" },
-    { "<C-c>oiL", mode = "n", desc = "[O]cto [I]ssue [L]ist", "<cmd>Octo issue list<cr>" },
-    { "<C-c>oic", mode = "n", desc = "[O]cto [I]ssue [C]reate", "<cmd>Octo issue create<cr>" },
+    { "<C-c>oiL", mode = "n", desc = "[O]cto [I]ssue [L]ist",    "<cmd>Octo issue list<cr>" },
+    { "<C-c>oic", mode = "n", desc = "[O]cto [I]ssue [C]reate",  "<cmd>Octo issue create<cr>" },
   },
   opts = {
     suppress_missing_scope = {
