69d422dd5b613e5a6461a6453925d629b29d5418
Pablo Fonseca <pablofonseca777@gmail.com>
2024-07-23 22:04:01 +0100

    feat(hammerspoon): use Alacritty as default shell

--- hammerspoon/lua/window.lua
+++ hammerspoon/lua/window.lua
@@ -4,44 +4,44 @@ local wm = require("window-management")
 local hotkey = hs.hotkey.bind
 
 hotkey(u.master, "l", function()
-  wm.moveWindowToPosition(wm.screenPositions.right)
+	wm.moveWindowToPosition(wm.screenPositions.right)
 end)
 
 hotkey(u.master, "h", function()
-  wm.moveWindowToPosition(wm.screenPositions.left)
+	wm.moveWindowToPosition(wm.screenPositions.left)
 end)
 
 hotkey(u.master, "j", function()
-  wm.moveWindowToPosition(wm.screenPositions.bottom)
+	wm.moveWindowToPosition(wm.screenPositions.bottom)
 end)
 
 hotkey(u.master, "k", function()
-  wm.moveWindowToPosition(wm.screenPositions.top)
+	wm.moveWindowToPosition(wm.screenPositions.top)
 end)
 
 hotkey(u.master, "o", function()
-  wm.windowMaximize(0)
+	wm.windowMaximize(0)
 end)
 
 hotkey(u.alt, "l", function()
-  hs.window.focusedWindow():moveOneScreenEast()
+	hs.window.focusedWindow():moveOneScreenEast()
 end)
 
 hotkey(u.alt, "h", function()
-  hs.window.focusedWindow():moveOneScreenWest()
+	hs.window.focusedWindow():moveOneScreenWest()
 end)
 
 --- Focus the app
 --- @param key string key to bind the hotkey
 --- @param appName string name of the app to be focused
 local function focusApp(key, appName)
-  return hotkey(u.master, key, function()
-    return hs.application.launchOrFocus(appName)
-  end)
+	return hotkey(u.master, key, function()
+		return hs.application.launchOrFocus(appName)
+	end)
 end
 
 focusApp("b", "Arc")
-focusApp("t", "iTerm")
+focusApp("t", "Alacritty")
 focusApp("s", "Slack")
 focusApp("n", "Obsidian")
 focusApp("f", "Firefox")
