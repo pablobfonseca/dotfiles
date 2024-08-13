01ee8726f83c6fc96ad3675e4ac708d1e48c7697
Pablo Fonseca <pablofonseca777@gmail.com>
2024-08-13 12:40:05 +0100

    fix(hammerspoon): comment window movements
    testing aerospace instead

--- hammerspoon/lua/window.lua
+++ hammerspoon/lua/window.lua
@@ -3,33 +3,33 @@ local wm = require("window-management")
 
 local hotkey = hs.hotkey.bind
 
-hotkey(u.master, "l", function()
-	wm.moveWindowToPosition(wm.screenPositions.right)
-end)
-
-hotkey(u.master, "h", function()
-	wm.moveWindowToPosition(wm.screenPositions.left)
-end)
-
-hotkey(u.master, "j", function()
-	wm.moveWindowToPosition(wm.screenPositions.bottom)
-end)
-
-hotkey(u.master, "k", function()
-	wm.moveWindowToPosition(wm.screenPositions.top)
-end)
-
-hotkey(u.master, "o", function()
-	wm.windowMaximize(0)
-end)
-
-hotkey(u.alt, "l", function()
-	hs.window.focusedWindow():moveOneScreenEast()
-end)
-
-hotkey(u.alt, "h", function()
-	hs.window.focusedWindow():moveOneScreenWest()
-end)
+-- hotkey(u.master, "l", function()
+-- 	wm.moveWindowToPosition(wm.screenPositions.right)
+-- end)
+--
+-- hotkey(u.master, "h", function()
+-- 	wm.moveWindowToPosition(wm.screenPositions.left)
+-- end)
+--
+-- hotkey(u.master, "j", function()
+-- 	wm.moveWindowToPosition(wm.screenPositions.bottom)
+-- end)
+--
+-- hotkey(u.master, "k", function()
+-- 	wm.moveWindowToPosition(wm.screenPositions.top)
+-- end)
+--
+-- hotkey(u.master, "o", function()
+-- 	wm.windowMaximize(0)
+-- end)
+--
+-- hotkey(u.alt, "l", function()
+-- 	hs.window.focusedWindow():moveOneScreenEast()
+-- end)
+--
+-- hotkey(u.alt, "h", function()
+-- 	hs.window.focusedWindow():moveOneScreenWest()
+--end)
 
 --- Focus the app
 --- @param key string key to bind the hotkey
