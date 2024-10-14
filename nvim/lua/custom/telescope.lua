28f3a96f3cdfa86b55bb9163333904454049377b
Pablo Fonseca <pablofonseca777@gmail.com>
2024-10-15 00:04:48 +0100

    feat(nvim): add ability to select multiple files

--- nvim/lua/custom/telescope.lua
+++ nvim/lua/custom/telescope.lua
@@ -3,6 +3,21 @@ local lga_actions = require "telescope-live-grep-args.actions"
 local builtin = require "telescope.builtin"
 local z_utils = require "telescope._extensions.zoxide.utils"
 
+local select_one_or_multi = function(prompt_bufnr)
+  local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+  local multi = picker:get_multi_selection()
+  if not vim.tbl_isempty(multi) then
+    require("telescope.actions").close(prompt_bufnr)
+    for _, j in pairs(multi) do
+      if j.path ~= nil then
+        vim.cmd(string.format("%s %s", "edit", j.path))
+      end
+    end
+  else
+    require("telescope.actions").select_default(prompt_bufnr)
+  end
+end
+
 telescope.setup {
   defaults = {
     vimgrep_arguments = {
@@ -14,6 +29,11 @@ telescope.setup {
       "--column",
       "--smart-case",
       "--hidden",
+    },
+    mappings = {
+      i = {
+        ["<CR>"] = select_one_or_multi,
+      },
     },
     extensions = {
       live_grep_args = {
