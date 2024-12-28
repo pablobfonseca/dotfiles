e326124f1c591348a447ae1cbb5fddc99303d0d9
Pablo Fonseca <pablofonseca777@gmail.com>
2024-12-28 16:34:51 +0000

    fix(nvim): improve completion

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -20,6 +20,8 @@ return {
       accept = { auto_brackets = { enabled = true } },
       documentation = {
         auto_show = true,
+        auto_show_delay_ms = 250,
+        treesitter_highlighting = true,
         window = {
           border = "single",
           scrollbar = false,
@@ -31,9 +33,19 @@ return {
         auto_show = function(ctx)
           return ctx.mode ~= "cmdline" or not vim.tbl_contains({ "/", "?" }, vim.fn.getcmdtype())
         end,
+        cmdline_position = function()
+          if vim.g.ui_cmdline_pos ~= nil then
+            local pos = vim.g.ui_cmdline_pos
+            return { pos[1] - 1, pos[2] }
+          end
+          local height = (vim.o.cmdheight == 0) and 1 or vim.o.cmdheight
+          return { vim.o.lines - height, 0 }
+        end,
       },
       list = {
-        selection = "auto_insert",
+        selection = function(ctx)
+          return ctx.mode == "cmdline" and "auto_insert" or "preselect"
+        end,
       },
     },
     sources = {
@@ -43,7 +55,8 @@ return {
         buffer = {
           name = "Buffer",
           module = "blink.cmp.sources.buffer",
-          min_keyword_length = 3,
+          min_keyword_length = 5,
+          max_items = 5,
         },
       },
     },
