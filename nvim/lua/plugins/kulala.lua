e2c9bdc7cf585d6477de305f16df2a4332bd4255
Pablo Fonseca <pablofonseca777@gmail.com>
2026-07-23 16:06:45 +0100

    fix(nvim): fix kulala

--- nvim/lua/plugins/kulala.lua
+++ nvim/lua/plugins/kulala.lua
@@ -1,12 +1,48 @@
 return {
   "mistweaverco/kulala.nvim",
-  ft = "http",
+  event = { "SessionLoadPost", "VimLeavePre" },
+  ft = { "http", "rest", "javascript", "lua" },
   keys = {
-    { "<leader>rm", function() require("kulala").scratchpad() end, desc = "Open request scratchpad" },
-    { "<C-c><C-c>", function() require("kulala").run() end, desc = "Run request" },
-    { "<C-c>kt", function() require("kulala").toggle_view() end, desc = "Kulala Toggle View" },
-    { "<C-c>ks", function() require("kulala").search() end, desc = "Kulala Search" },
-    { "<C-c>kS", function() require("kulala").set_selected_env() end, desc = "Kulala select env" },
+    {
+      "<leader>rm",
+      function()
+        require("kulala").scratchpad()
+      end,
+      desc = "Open request scratchpad",
+    },
+    {
+      "<C-c><C-c>",
+      function()
+        require("kulala").run()
+      end,
+      desc = "Run request",
+    },
+    {
+      "<C-c>kt",
+      function()
+        require("kulala").toggle_view()
+      end,
+      desc = "Kulala Toggle View",
+    },
+    {
+      "<C-c>ks",
+      function()
+        require("kulala").search()
+      end,
+      desc = "Kulala Search",
+    },
+    {
+      "<C-c>kS",
+      function()
+        require("kulala").set_selected_env()
+      end,
+      desc = "Kulala select env",
+    },
   },
-  opts = {},
+  opts = {
+    treesitter = {
+      enable = true,
+      cli_path = "tree-sitter",
+    },
+  },
 }
